20de90c2d687afaaf4d66181709dbe4ec1ba275f
David Martinez <47610359+dvdmrtnz@users.noreply.github.com>
2025-09-04 21:18:35 +0200

    feat: Add dark filter to tiles

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 import {LeafletMap, TileLayer} from 'leaflet';
 
+const TILE_DARKNESS = 'brightness(30%)';
+
 const map = new LeafletMap('map').setView([51.505, -0.09], 13);
 
 const tiles = new TileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+// Apply a dark filter to the tile layer
+tiles.getContainer().style.filter = TILE_DARKNESS;
